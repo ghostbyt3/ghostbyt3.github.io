249bb3fff707897f9070c4f0f67a275968598372
root <nikhiljohn2104@gmail.com>
2025-09-09 23:07:23 +0530

    Adding Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
 import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
